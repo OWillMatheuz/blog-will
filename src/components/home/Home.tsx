a850f822ded0d937c1cb9095d4eca543114a0a95
Will Matheus <dev.owillmatheus@gmail.com>
2023-02-27 11:57:55 -0300

    atualização dos componentes react

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://github.com/OWillMatheuz.png" alt="github" className="img" />
+        </>
+    );
+}
+export default Home;
