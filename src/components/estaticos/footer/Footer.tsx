593d2762dfa98033f07b74393323dd44b5ce631f
Will Matheus <dev.owillmatheus@gmail.com>
2023-03-09 12:00:42 -0300

    Atualização do footer

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -6,12 +6,19 @@ import GithubIcon from '@material-ui/icons/GitHub';
 import { Grid, Typography } from "@material-ui/core";
 import { Box } from "@mui/material";
 import './Footer.css';
+import { useSelector } from "react-redux";
+import { TokenState } from "../../../store/tokens/tokensReducer";
+
 
 
 function Footer(){
-    return (
-        <>
-        <Grid container  direction="row" justifyContent="center" alignItems="center">
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
+
+      var footerComponent;
+      if(token != ""){
+       footerComponent = <Grid container  direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
                     <Box className='box1'>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
@@ -42,7 +49,12 @@ function Footer(){
                     </Box>
                 </Grid>
             </Grid>
-        </>
+      }
+    return(
+        <>
+        {footerComponent}
+    </>
+
     )
 }
 export default Footer;
